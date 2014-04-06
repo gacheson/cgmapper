268f4c070bba0fbc740053e768af3c3629a84667
gacheson <geoffrey.acheson@earthlink.net>
2014-04-06 13:32:35 -0500

    Update clocks.py, fix not str error

--- clocks.py
+++ clocks.py
@@ -4,13 +4,13 @@ import api
 import gpu
 
 # Customize these
-card = '0,1'       # Card number, 0 is the first
-mem_min = '1400,1400' # The memory clock to start at
-mem_max = '1500,1500' # The memory clock to end at
-mem_step = '3'   # The interval to try memory clocks at (1 = all, 2 = every other, etc.)
-core_min = '1075,975' # The core clock to start at
-core_max = '1100,1050' # The core clock to end at
-core_step = '1'  # The interval to try core clocks
+card = 1 # Card number, 0 is the first
+mem_min = 1250 # The memory clock to start at
+mem_max = 1460 # The memory clock to end at
+mem_step = 3 # The interval to try memory clocks at (1 = all, 2 = every other, etc.)
+core_min = 830 # The core clock to start at
+core_max = 890 # The core clock to end at
+core_step = 1 # The interval to try core clocks
 desired_accuracy_in_mhs = 0.002 # The desired accuracy of samples in megahashes per second. .002 or .001 is recommended here
 half_cycle_write = True # The desired frequency of writes made to file. Half cycle (True) writes at core min and at core max. Full cycle (False) only writes at core min
 
