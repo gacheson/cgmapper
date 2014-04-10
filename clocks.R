a347bc9c426bc95bd8feefab435632b1046057da
Geoffrey Acheson <geoffrey.acheson@earthlink.net>
2014-04-10 14:32:25 -0500

    Update clocks.R, add multiple .csv file processing

--- clocks.R
+++ clocks.R
@@ -1,4 +1,10 @@
 library(ggplot2)
-infile <- read.csv(file="mhs.csv", header=TRUE,sep=",")
-out <- ggplot(data=infile, aes(x=mem, y=core, fill=mhs)) + geom_tile() + scale_fill_gradientn(colours = rainbow(7))
-ggsave(out,filename='clocks.png')
+files <- list.files(pattern=".csv$")
+g <- 0
+for(f in files)
+{
+    infile <- read.csv(paste(f),header=TRUE,sep=",")
+    out <- ggplot(data=infile, aes(x=mem, y=core, fill=mhs)) + geom_tile() + scale_fill_gradientn(colours = rainbow(7))
+    ggsave(out,filename=paste("clocks-", g, ".png", sep = ""))
+    g <- g + 1
+}
