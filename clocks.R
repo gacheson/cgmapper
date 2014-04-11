9bf87e5afb7c79d4cc67a66582eb988ce361bf52
Geoffrey Acheson <geoffrey.acheson@earthlink.net>
2014-04-10 19:12:31 -0500

    Update clocks.R, fix file naming

--- clocks.R
+++ clocks.R
@@ -1,10 +1,9 @@
 library(ggplot2)
 files <- list.files(pattern=".csv$")
-g <- 0
 for(f in files)
 {
-    infile <- read.csv(paste(f),header=TRUE,sep=",")
+    gpu <- gsub(".*-(.*)\\..*", "\\1", c(f))
+    infile <- read.csv(paste(f), header=TRUE, sep=",")
     out <- ggplot(data=infile, aes(x=mem, y=core, fill=mhs)) + geom_tile() + scale_fill_gradientn(colours = rainbow(7))
-    ggsave(out,filename=paste("clocks-", g, ".png", sep = ""))
-    g <- g + 1
+    ggsave(out,filename=paste("clocks-", gpu, ".png", sep = ""))
 }
